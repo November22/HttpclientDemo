4ce6456d81a9b3a6c0c8eceae9cc4bb8020d8039
sen.huang <sen.huang@yeepay.com>
2018-05-12 15:16:52 +0800

    uribuilder

--- src/test/java/HttpClientTest.java
+++ src/test/java/HttpClientTest.java
@@ -1,6 +1,8 @@
 import com.httpclient.HttpConnectionManager;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
@@ -8,6 +10,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.net.URI;
 
 /**
  * @author sen.huang
@@ -21,6 +25,20 @@ public class HttpClientTest {
     private HttpConnectionManager connectionManager;
 
     @Test
+    public void testURIBuilder() throws Exception{
+        URI uri = new URIBuilder()
+                .setScheme("https")
+                .setHost("www.baidu.com")
+                .setPath("baidu")
+                .setParameter("tn", "monline_3_dg")
+                .setParameter("ie", "utf-8")
+                .setParameter("wd", "httpclient+易踪")
+                .build();
+        HttpPost post = new HttpPost(uri);
+        System.out.println(post.getURI());
+    }
+
+    @Test
     public void testHttpClient() throws Exception{
         CloseableHttpClient httpClient = connectionManager.getHttpClient();
         HttpGet get = new HttpGet("https://blog.csdn.net/zhuwukai/article/details/78644484");
@@ -28,5 +46,7 @@ public class HttpClientTest {
         String string = EntityUtils.toString(response.getEntity());
         System.out.printf(string);
     }
+
+
 
 }
